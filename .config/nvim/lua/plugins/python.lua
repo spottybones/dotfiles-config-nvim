cb09c843812069ee4b956374e4599402d6736f49
Scott Burns <scott@lentigo.net>
2023-10-02 10:21:25 -0400

    Replace 'null-ls' With 'none-ls'
    'none-ls' is a community fork of 'null-ls', which is no longer
    maintained.

--- .config/nvim/lua/plugins/python.lua
+++ .config/nvim/lua/plugins/python.lua
@@ -19,7 +19,7 @@ return {
   },
   {
     -- enable black formatting for null-js
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
       table.insert(opts.sources, nls.builtins.formatting.black)
