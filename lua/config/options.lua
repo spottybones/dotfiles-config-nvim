c3c471c7f7afe3a7a76265329a6062044f0ab5cf
Scott Burns <scott@lentigo.net>
2025-05-09 16:39:32 -0400

    feat: let Nix home-manager set language providers
    Home-manager will enable/disable the Python, NodeJS, and Ruby providers.

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,7 +2,4 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.g.python3_host_prog = "~/.local/share/venvs/neovim/bin/python3"
-vim.g.loaded_ruby_provider = 0
 vim.g.loaded_perl_provider = 0
-vim.g.loaded_node_provider = 0
